2a37ca3b9532d882cba4b6a8fbe505f9fcffdcfe
JulitorK <julita.kriauciunaite@gmail.com>
2022-02-09 12:04:26 +0200

    UGN-192 chore - decouple button from modal

--- src/stories/Default.stories.tsx
+++ src/stories/Default.stories.tsx
@@ -1,6 +1,15 @@
 import { ReactSpreadsheetImport } from "../components/ReactSpreadsheetImport"
+import { Button, useDisclosure } from "@chakra-ui/react"
 export default {
   title: "React spreadsheet import",
 }
 
-export const Basic = () => <ReactSpreadsheetImport />
+export const Basic = () => {
+  const { isOpen, onOpen, onClose } = useDisclosure()
+  return (
+    <>
+      <Button onClick={onOpen}>Open modal</Button>
+      <ReactSpreadsheetImport isOpen={isOpen} onClose={onClose} />
+    </>
+  )
+}
