4f6b903f5186d9e372105d8e4688b24bdf642538
JulitorK <julita.kriauciunaite@gmail.com>
2022-02-14 12:45:16 +0200

    feature UGN-200 - added overlay

--- src/components/ReactSpreadsheetImport.tsx
+++ src/components/ReactSpreadsheetImport.tsx
@@ -8,6 +8,8 @@ import {
   ModalContent,
   ModalFooter,
   Button,
+  ModalOverlay,
+  useTheme,
 } from "@chakra-ui/react"
 
 type ReactSpreadsheetImportProps = {
@@ -17,22 +19,26 @@ type ReactSpreadsheetImportProps = {
 
 export const ReactSpreadsheetImport = ({ isOpen, onClose }: ReactSpreadsheetImportProps) => {
   return (
-    <ChakraProvider cssVarsRoot="#chakra-modal-rsi">
-      <Modal isOpen={isOpen} onClose={onClose} id={"rsi"} size="full">
-        <ModalContent>
-          <ModalHeader>Modal Title</ModalHeader>
-          <ModalCloseButton />
-          <ModalBody>
-            <UploadFlow />
-          </ModalBody>
-          <ModalFooter>
-            <Button colorScheme="blue" mr={3} onClick={onClose}>
-              Close
-            </Button>
-            <Button variant="ghost">Secondary Action</Button>
-          </ModalFooter>
-        </ModalContent>
-      </Modal>
+    <ChakraProvider>
+      {/* Needed to override RSI theme but not the rest of chakra theme */}
+      <ChakraProvider cssVarsRoot="#chakra-modal-rsi">
+        <Modal isOpen={isOpen} onClose={onClose} id="rsi" size="6xl" closeOnEsc={false} closeOnOverlayClick={false}>
+          <ModalOverlay />
+          <ModalContent>
+            <ModalHeader>Modal Title</ModalHeader>
+            <ModalCloseButton />
+            <ModalBody>
+              <UploadFlow />
+            </ModalBody>
+            <ModalFooter>
+              <Button colorScheme="blue" mr={3} onClick={onClose}>
+                Close
+              </Button>
+              <Button variant="ghost">Secondary Action</Button>
+            </ModalFooter>
+          </ModalContent>
+        </Modal>
+      </ChakraProvider>
     </ChakraProvider>
   )
 }
