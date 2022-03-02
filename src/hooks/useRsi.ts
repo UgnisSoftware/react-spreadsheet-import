4b95700cb6079b1b84c5cf72b340cdc76bed598e
JulitorK <julita.kriauciunaite@gmail.com>
2022-03-02 16:18:58 +0200

    feat UGN-246 - simplify TS type

--- src/hooks/useRsi.ts
+++ src/hooks/useRsi.ts
@@ -1,6 +1,6 @@
 import { useContext } from "react"
 import { RsiContext } from "../components/Providers"
 import type { RsiProps } from "../types"
+import type { MarkRequired } from "ts-essentials"
 
-export const useRsi = <T extends string>() =>
-  useContext<Omit<RsiProps<T>, "autoMapDistance"> & { autoMapDistance: number }>(RsiContext)
+export const useRsi = <T extends string>() => useContext<MarkRequired<RsiProps<T>, "autoMapDistance">>(RsiContext)
