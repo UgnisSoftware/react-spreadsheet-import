3641b44e3fa276ff995fbe69dbc5f956569af7d3
JulitorK <julita.kriauciunaite@gmail.com>
2022-03-11 18:41:32 +0200

    feat UGN-248 - centralized theme in SelectSheet step

--- src/steps/SelectSheetStep/SelectSheetStep.tsx
+++ src/steps/SelectSheetStep/SelectSheetStep.tsx
@@ -1,4 +1,4 @@
-import { Heading, ModalBody, Radio, RadioGroup, Stack } from "@chakra-ui/react"
+import { Heading, ModalBody, Radio, RadioGroup, Stack, useStyleConfig, Text } from "@chakra-ui/react"
 import { useState } from "react"
 import { ContinueButton } from "../../components/ContinueButton"
 import { useRsi } from "../../hooks/useRsi"
@@ -11,18 +11,17 @@ type SelectSheetProps = {
 export const SelectSheetStep = ({ sheetNames, onContinue }: SelectSheetProps) => {
   const { translations } = useRsi()
   const [value, setValue] = useState(sheetNames[0])
+  const styles = useStyleConfig("SelectSheetStep") as any
 
   return (
     <>
       <ModalBody alignItems="center" justifyContent="center" p={8} flex={1}>
-        <Heading size="lg" mb={8}>
-          {translations.uploadStep.selectSheet.title}
-        </Heading>
+        <Heading {...styles.heading}>{translations.uploadStep.selectSheet.title}</Heading>
         <RadioGroup onChange={(value) => setValue(value)} value={value}>
           <Stack spacing={8}>
             {sheetNames.map((sheetName) => (
-              <Radio value={sheetName} key={sheetName}>
-                {sheetName}
+              <Radio value={sheetName} key={sheetName} {...styles.radio}>
+                <Text {...styles.radioLabel}>{sheetName}</Text>
               </Radio>
             ))}
           </Stack>
