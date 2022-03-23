4fd5913f1b99a4b79dda69b4ab7487c6d4fbca3b
masiulis <karolis.masiulis@ugnis.com>
2022-03-23 08:42:54 +0000

    Deploying to gh-pages from @ UgnisSoftware/react-spreadsheet-import@d42505dccc23bcabc0abc364a35ec5feb1142932 🚀

--- src/steps/MatchColumnsStep/utils/uniqueEntries.ts
+++
@@ -1,8 +0,0 @@
-import uniqBy from "lodash/uniqBy"
-import type { MatchColumnsProps, MatchedOptions } from "../MatchColumnsStep"
-
-export const uniqueEntries = <T>(data: MatchColumnsProps["data"], index: number): Partial<MatchedOptions<T>>[] =>
-  uniqBy(
-    data.map((row) => ({ entry: row[index] })),
-    "entry",
-  ).filter(({ entry }) => !!entry)
