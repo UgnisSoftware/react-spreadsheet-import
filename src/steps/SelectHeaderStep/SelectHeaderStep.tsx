1b31e1fcfded5db3d17bd5c346ba257bfd9b4a8d
Tyler Jones <tyler@trybadge.com>
2023-07-27 11:35:38 -0700

    fix header select display issue on safari
    Setting height to 0px causes the header selection table not to show on Safari.
    
    fixes #179

--- src/steps/SelectHeaderStep/SelectHeaderStep.tsx
+++ src/steps/SelectHeaderStep/SelectHeaderStep.tsx
@@ -32,7 +32,7 @@ export const SelectHeaderStep = ({ data, onContinue }: SelectHeaderProps) => {
     <>
       <ModalBody pb={0}>
         <Heading {...styles.heading}>{translations.selectHeaderStep.title}</Heading>
-        <Box h={0} flexGrow={1}>
+        <Box flexGrow={1}>
           <SelectHeaderTable data={data} selectedRows={selectedRows} setSelectedRows={setSelectedRows} />
         </Box>
       </ModalBody>
