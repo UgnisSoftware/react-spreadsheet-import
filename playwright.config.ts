8197b12d9403676edf8951aac467714b384583a1
masiulis <karolis.masiulis@ugnis.com>
2023-10-19 14:20:05 +0300

    Run tests on dev mode storybook

--- playwright.config.ts
+++ playwright.config.ts
@@ -71,7 +71,7 @@ export default defineConfig({
   /* Run your local dev server before starting the tests */
   webServer: {
      command: 'npm run start',
-     url: 'http://localhost:6006/iframe.html?args=&id=match-columns-steps--basic&viewMode=story',
+     url: 'http://localhost:6006',
      reuseExistingServer: !process.env.CI,
   },
 });
