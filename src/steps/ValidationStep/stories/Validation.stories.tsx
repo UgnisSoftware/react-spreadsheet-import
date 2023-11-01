df0675c15b6ef485f7952400b4861501d9cc67a3
masiulis <karolis.masiulis@ugnis.com>
2023-11-01 12:15:48 +0200

    Fix validation step story to run validations before initial render

--- src/steps/ValidationStep/stories/Validation.stories.tsx
+++ src/steps/ValidationStep/stories/Validation.stories.tsx
@@ -3,6 +3,7 @@ import { ValidationStep } from "../ValidationStep"
 import { Providers } from "../../../components/Providers"
 import { defaultTheme } from "../../../ReactSpreadsheetImport"
 import { ModalWrapper } from "../../../components/ModalWrapper"
+import { addErrorsAndRunHooks } from "../utils/dataMutations"
 
 export default {
   title: "Validation Step",
@@ -12,11 +13,14 @@ export default {
 }
 
 const file = new File([""], "file.csv")
+const data = await addErrorsAndRunHooks(editableTableInitialData, mockRsiValues.fields)
 
-export const Basic = () => (
-  <Providers theme={defaultTheme} rsiValues={mockRsiValues}>
-    <ModalWrapper isOpen={true} onClose={() => {}}>
-      <ValidationStep initialData={editableTableInitialData} file={file} />
-    </ModalWrapper>
-  </Providers>
-)
+export const Basic = () => {
+  return (
+    <Providers theme={defaultTheme} rsiValues={mockRsiValues}>
+      <ModalWrapper isOpen={true} onClose={() => {}}>
+        <ValidationStep initialData={data} file={file} />
+      </ModalWrapper>
+    </Providers>
+  )
+}
